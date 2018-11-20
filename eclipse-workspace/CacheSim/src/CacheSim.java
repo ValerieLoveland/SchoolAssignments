8191fd9288acac39fcb05a126c43ddc80853a15d
Valerie Loveland <valerieloveland@gmail.com>
2018-11-20 14:35:31 -0500

    About the start work on the displays

--- eclipse-workspace/CacheSim/src/CacheSim.java
+++ eclipse-workspace/CacheSim/src/CacheSim.java
@@ -8,7 +8,7 @@ public class CacheSim {
 		char[] directionsArray = {'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'D', 'W', 'W', 'R', 'D', 'R', 'R', 'D', 'R', 'R', 'R', 'R', 'D'};
 		
 		int cache[][];
-		cache = new int[17][22];
+		cache = new int[18][22];
 		int MM[]= new int[2048];
 		String cacheHead[];
 		cacheHead = new String[8];
@@ -41,8 +41,8 @@ public class CacheSim {
 				MM[i]=i;
 				
 			//I don't need this to print so I am taking this out for now. this is to debug********************************
-				System.out.printf("%02x",MM[i]);//This is to just print the value in each element in MM inhex*********************
-				System.out.println(); 
+//				System.out.printf("%02x",MM[i]);//This is to just print the value in each element in MM inhex*********************
+		//		System.out.println(); 
 			}
 		
 		}	/*for(int i = 0; i<=0xF; i++){
@@ -81,7 +81,7 @@ public class CacheSim {
 		//System.out.print(cache[0][0]);
 		for(int i = 0x0; i<=0xff; i++){
 
-		    for(int j = 0x0; j<20; j++){
+		    for(int j = 0x0; j<0x14; j++){
 		       // System.out.printf("%2x\t",cache[i][j]+"     ");//this is too big of tabs************************
 		        System.out.printf("%2x",cache[i][j]);
 		        //System.out.print("     ");
@@ -163,54 +163,52 @@ public class CacheSim {
 		
 			}}		
 			
-	
-	
-			
-			
 
-
-
-	//make write here**********************************
- 
+	//make write here***************************************************************************************************************************
+ //*********************************************************************************************************************************************
+//***********************************************************************************************************************************************
  if (directionsArray[d]=='W'){
 	 int addyInput = writeArray[w];
-		
-		System.out.printf("0x%03X", addyInput);
+	 System.out.println("What address would you want to write to?");		
+	 writeAddress=writeArray[w];	
+	 System.out.printf("0x%03X", writeAddress);
+	 System.out.println();
+	 System.out.println("What data would you want to write at that address?");
+	 int writeData=writeArray[w+1];
+	 System.out.printf("0x%03X", writeData);
+	 System.out.println();
  
-int offset=(addyInput & 0x00f);
-int tag=(addyInput & 0xf00)>>>8;
-int slots = (addyInput & 0x0f0)>>>4;
+int offset=(writeAddress & 0x00f);
+int tag=(writeAddress & 0xf00)>>>8;
+int slots = (writeAddress & 0x0f0)>>>4;
+int tagAndSlot=(writeAddress & 0xff0);
+int slotAndOffset=(writeAddress & 0x0ff);
+int wholeblockstarts = tagAndSlot;	
 
 int validBit =cache[slots][2];
-System.out.println("What address would you want to write to?");
-writeAddress=writeArray[w];
-System.out.println("What data would you want to write at that address?");
-int writeData=writeArray[w+1];
-w=w+1;
-
-
-
 
 if ((cache[slots][2] == 0) || (tag != cache[slots][3])) {
+	for (int i=4;i<20; i++){
 hitOrMiss= "Miss";
+cache[slots][i]= MM[wholeBlockStarts];
+wholeBlockStarts=wholeBlockStarts +1;
+	}
 System.out.println("Value "+ inputData + "has been written to address"+ "(cache " + hitOrMiss + ")");
 //MM[x5A1234]=6. 
-//dirtyBit= 1;
-validBit=1;
+cache[slots][1]= 1; //dirty bit
+cache[slots][2]= 1; //valid bit
+cache[slots][3]=tag;
 }
 else {
-hitOrMiss="hit";
+hitOrMiss="Hit";
 cache[row][column]=inputData;
-System.out.println("Value "+ inputData + "has been written to address"+ "(cache" + hitOrMiss + ")");
-//MM[x5A1234]=6. 
-//dirtyBit= 1;
-validBit=1;
-cache[slots][2]=1;//valid bit
-validBit=1;
-cache[slots][3]=tag;
-cache[slots][1]=1; //dirtyBit
-				
-}
+System.out.println("Value "+ inputData + "has been written to address"+ "(cache " + hitOrMiss + ")");
+//MM[addyInput]=writeArray[w]; 
+cache[slots][1]= 1; //dirty bit
+cache[slots][2]= 1; //valid bit
+cache[slots][3]=tag;}
+d++;
+w++;
 }}}
 
 /*this is to writeback the cache into the MM (what is i and j here?)
@@ -223,11 +221,5 @@ w++;
 }}
 
 
-
-/*this is to copy the MM and put it into the cache
-copiedMM=Arrays.copyOfRange(MM, 2, MM.16); // returns {30, 40, 50} (length = 5)
-Arrays.copyOfRange(arr, 2, arr.length); // returns {30, 40, 50} (length = 5)
-cache[i][j]=copied MM[i]; (what is i and j here)
-}
 
 */
